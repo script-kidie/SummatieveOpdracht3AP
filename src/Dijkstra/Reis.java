f1e2d1ecb283a31d7605830effaf635fc7f797b3
script-kidie <74268141+script-kidie@users.noreply.github.com>
2021-05-27 21:40:06 +0200

    added documentation

--- src/Dijkstra/Reis.java
+++ src/Dijkstra/Reis.java
@@ -18,14 +18,14 @@ public class Reis implements Comparable<PriorityQueue<Stap>> {
 
     @Override
     public int compareTo(PriorityQueue<Stap> steps){
-        Stap current = steps.poll();
-        if(current == null){
+        Stap current = steps.poll(); //fetches the first step from the PQ
+        if(current == null){ //checks if the Pq is empty
             System.out.println("deze reis in niet mogelijk");
-            return 21;
+            return 21; // return 21 because its fun
         } else {
             for (Stap s : steps) {
-                if (s.getValue() < current.getValue()) {
-                    current = s;
+                if (s.getValue() < current.getValue()) { // determine if the value of the current step is higher then the original
+                    current = s; // make the current step the one whe want to return
                 }
             }
 
